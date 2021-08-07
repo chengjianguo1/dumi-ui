a96a33c41d035b38b2f62cd03d77d6c4b439f007
wb.chengjianguo <whhjianguo@163.com>
2021-08-07 21:40:46 +0800

    'umirc.ts'

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'dumi';
 
+let BaseUrl = '/dumi-ui';
+
 export default defineConfig({
   title: 'dumi-ui',
   favicon:
@@ -8,6 +10,8 @@ export default defineConfig({
   outputPath: 'docs-dist',
   mode: 'site',
   // more config: https://d.umijs.org/config
+  base: BaseUrl,
+  publicPath: BaseUrl + '/',
   lessLoader: {
     module: {
       rules: [
