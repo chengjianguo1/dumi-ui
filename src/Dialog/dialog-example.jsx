2b2f1c3877c9513dbd22cb3d1b0bda5aaa2f54d8
chengjianguo <whhjianguo@163.com>
2023-07-15 14:13:25 +0800

    'feat:蒙版'

--- src/Dialog/dialog-example.jsx
+++ src/Dialog/dialog-example.jsx
@@ -3,28 +3,30 @@ import { Dialog } from 'dumi-ui';
 import './index.less';
 
 export default () => {
-    const show = () => {
-        const dialog = Dialog.show({
-            title:"dialog title",
-            content: <div>content</div>,
-            hasMask: false,
-            visible: true,
-            onClose: () => {
-              console.log('onClose')
-            },
-            onSuccess:() => {
-              console.log('onsuccess')
-            },
-            footerAlign:"right",
-        })
-        setTimeout(() => {
-            dialog.hide();
-        }, 2000);
-    }
+  const show = () => {
+    const dialog = Dialog.show({
+      title: 'dialog title',
+      content: <div>content</div>,
+      hasMask: false,
+      visible: true,
+      onClose: () => {
+        console.log('onClose');
+      },
+      onSuccess: () => {
+        console.log('onsuccess');
+      },
+      footerAlign: 'right',
+    });
+    setTimeout(() => {
+      dialog.hide();
+    }, 2000);
+  };
 
-    return (
-        <div>
-            <span className="dialog-btn-example" onClick={show}>show Dialog</span>
-        </div>
-    )
-}
+  return (
+    <div>
+      <span className="dialog-btn-example" onClick={show}>
+        show Dialog
+      </span>
+    </div>
+  );
+};
